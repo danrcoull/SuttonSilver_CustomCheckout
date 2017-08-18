be68a85e49f9832239da811df7406dc2c8e8f4a5
Daniel Coull <d.coull@suttonsilver.co.uk>
2017-08-18 16:34:06 +0100

    theme2

--- Plugin/Magento/Customer/CustomerPlugin.php
+++ Plugin/Magento/Customer/CustomerPlugin.php
@@ -14,11 +14,6 @@ class CustomerPlugin
        $this->request = $request;
     }
 
-	public function isJson($string) {
-		json_decode($string);
-		return (json_last_error() == JSON_ERROR_NONE);
-	}
-
 
 	public function beforeSave(CustomerRepository $subject, CustomerInterface $customer) {
 
