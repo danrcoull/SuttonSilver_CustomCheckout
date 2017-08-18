091dc48b193a0a504b47490bef37819579a7e9cc
Daniel Coull <d.coull@suttonsilver.co.uk>
2017-08-18 15:12:04 +0100

    errors

--- Plugin/Magento/Customer/CustomerPlugin.php
+++ Plugin/Magento/Customer/CustomerPlugin.php
@@ -18,7 +18,7 @@ class CustomerPlugin
 
 	public function beforeSave(CustomerRepository $subject, CustomerInterface $customer) {
 
-		die(var_dump($customer->getAddresses()));
+		die(var_dump($customer->getAddresses()->getData()));
 		$addresses = $this->request->getPost( 'address' );
 		$id        = 0;
 		if ( $addresses ) {
