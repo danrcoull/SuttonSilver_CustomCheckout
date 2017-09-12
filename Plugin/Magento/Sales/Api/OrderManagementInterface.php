27277ce2f66dc5b47a72f6c6498dd4db1755da1b
Daniel Coull <d.coull@suttonsilver.co.uk>
2017-09-12 12:40:45 +0100

    after success

---
+++ Plugin/Magento/Sales/Api/OrderManagementInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+class OrderManagementInterface {
+
+	private $customerSession;
+
+	public function __construct(
+		\Magento\Customer\Model\Session $customerSession
+	) {
+
+		$this->customerSession = $customerSession;
+	}
+
+	public function afterPlace(\Magento\Sales\Api\Data\OrderInterface $subject)
+	{
+		$this->customerSession->logout();
+		return $subject;
+	}
+
+}
