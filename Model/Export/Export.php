c7037fd7b836edabc1d9df44d87272e094869b44
Daniel Coull <d.coull@suttonsilver.co.uk>
2017-09-22 12:21:57 +0100

    log country

--- Model/Export/Export.php
+++ Model/Export/Export.php
@@ -167,8 +167,7 @@ class Export extends \SuttonSilver\CustomCheckout\Model\Export\ExportAbstract
 					{
 						$destination = $shippingAddress->getCountry() == 'GB' ? 1 : 0;
 						$this->getShippingPrice($order->getAllItems(), $destination);
-						$this->logger->info(print_r($this->_breakdown,true));
-						$this->logger->info( $shippingAddress->getCountry());
+						$this->logger->info(print_r($shippingAddress->getCountry(),true));
 
 						//$shippingAddress = $this->addressRepository->getById( ( ( $address2 !== false ) ? $address2 : "" );->getId() );
 
