59b43bf5d3bda5e0e0725e0fbd27a3af353e3a7d
Daniel Coull <d.coull@suttonsilver.co.uk>
2017-09-25 11:37:55 +0100

    log street return

--- Model/Export/Export.php
+++ Model/Export/Export.php
@@ -27,7 +27,7 @@ class Export extends \SuttonSilver\CustomCheckout\Model\Export\ExportAbstract
 	    $aMatch         = array();
 	    $pattern        = '/(?:unit\s+)?(?:Unit\s?)?(?:[0-9]+\s?-\s?[0-9]+|[0-9]+[a-zA-Z]{0,4})/';
 	    $matchResult    = preg_match($pattern, $street, $aMatch);
-	    $number         = (isset($aMatch[0][0])) ? $aMatch[0][0] : '';
+	    $number         = (isset($aMatch[0])) ? $aMatch[0] : '';
 	    $street         = str_replace($number, $street, "");
 
 
